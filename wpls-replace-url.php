b03d9968c7d7b410a12e4bea67e9a5acfa91d134
Kevin <oryc9x@gmail.com>
2017-01-21 17:21:44 +0700

    Update wpls-replace-url.php

--- wpls-replace-url.php
+++ wpls-replace-url.php
@@ -1,7 +1,13 @@
 <?php
 /**
- * Plugin Name: Replace URL
- * Author: WPLS
+ * Plugin Name: WP URL Flexible
+ * Plugin URI: https://github.io/dungps/wp-url-flexible
+ * Author: DungPS
+ * Author URI: dungps.com
  * Version: 1.0.0
  */
 
+function wp_url_flexible( $url ) {
+	$host_domain = 
+  $current_domain = $_SERVER['SERVER_HOST'];
+}
